64ed97e1ee1d85728cb43f0776d4bf503e7d12d9
ALONGOD <alongoddard1@gmail.com>
2024-07-31 09:41:26 +0300

    Use async await

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import path from 'path'
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import path from 'path';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-import { loggerService } from './services/logger.service.js'
-import { toyService } from './services/toy.service.js'
+import { loggerService } from './services/logger.service.js';
+import { toyService } from './services/toy.service.js';
 
-const app = express()
+const app = express();
 
 const corsOptions = {
   origin: [
@@ -20,94 +20,90 @@ const corsOptions = {
     'http://127.0.0.1:5174',
   ],
   credentials: true,
-}
-// App Configuration
-app.use(express.static('public'))
-app.use(cookieParser()) // for res.cookies
-app.use(express.json()) // for req.body
-app.use(cors(corsOptions))
+};
 
+// App Configuration
+app.use(express.static('public'));
+app.use(cookieParser()); // for res.cookies
+app.use(express.json()); // for req.body
+app.use(cors(corsOptions));
 
 // **************** Toys API ****************:
 // GET toys
-app.get('/api/toy', (req, res) => {
-  const { filterBy = {}, sortBy = {}, pageIdx } = req.query
-  toyService.query(filterBy, sortBy, pageIdx)
-    .then(toys => {
-      res.send(toys)
-    })
-    .catch(err => {
-      loggerService.error('Cannot load toys', err)
-      res.status(400).send('Cannot load toys')
-    })
-})
-
-app.get('/api/toy/:toyId', (req, res) => {
-  const { toyId } = req.params
-  toyService.get(toyId)
-    .then(toy => {
-      res.send(toy)
-    })
-    .catch(err => {
-      loggerService.error('Cannot get toy', err)
-      res.status(400).send(err)
-    })
-})
-
-app.post('/api/toy', (req, res) => {
-  const { name, price, labels } = req.body
-  const toy = {
-    name,
-    price: +price,
-    labels,
+app.get('/api/toy', async (req, res) => {
+  try {
+    const { filterBy = {}, sortBy = {}, pageIdx } = req.query;
+    const toys = await toyService.query(filterBy, sortBy, pageIdx);
+    res.send(toys);
+  } catch (err) {
+    loggerService.error('Cannot load toys', err);
+    res.status(400).send('Cannot load toys');
   }
-  toyService.save(toy)
-    .then(savedToy => {
-      res.send(savedToy)
-    })
-    .catch(err => {
-      loggerService.error('Cannot add toy', err)
-      res.status(400).send('Cannot add toy')
-    })
-})
+});
 
-app.put('/api/toy', (req, res) => {
-  const { name, price, _id, labels } = req.body
-  const toy = {
-    _id,
-    name,
-    price: +price,
-    labels,
+app.get('/api/toy/:toyId', async (req, res) => {
+  try {
+    const { toyId } = req.params;
+    const toy = await toyService.get(toyId);
+    res.send(toy);
+  } catch (err) {
+    loggerService.error('Cannot get toy', err);
+    res.status(400).send(err.message || 'Cannot get toy');
   }
-  toyService.save(toy)
-    .then(savedToy => {
-      res.send(savedToy)
-    })
-    .catch(err => {
-      loggerService.error('Cannot update toy', err)
-      res.status(400).send('Cannot update toy')
-    })
-})
+});
 
-app.delete('/api/toy/:toyId', (req, res) => {
-  const { toyId } = req.params
-  toyService.remove(toyId)
-    .then(msg => {
-      res.send({ msg, toyId })
-    })
-    .catch(err => {
-      loggerService.error('Cannot delete toy', err)
-      res.status(400).send('Cannot delete toy, ' + err)
-    })
-})
+app.post('/api/toy', async (req, res) => {
+  try {
+    const { name, price, labels } = req.body;
+    const toy = {
+      name,
+      price: +price,
+      labels,
+    };
+    const savedToy = await toyService.save(toy);
+    res.send(savedToy);
+  } catch (err) {
+    loggerService.error('Cannot add toy', err);
+    res.status(400).send('Cannot add toy');
+  }
+});
+
+app.put('/api/toy', async (req, res) => {
+  try {
+    const { name, price, _id, labels } = req.body;
+    const toy = {
+      _id,
+      name,
+      price: +price,
+      labels,
+    };
+    const savedToy = await toyService.save(toy);
+    res.send(savedToy);
+  } catch (err) {
+    loggerService.error('Cannot update toy', err);
+    res.status(400).send('Cannot update toy');
+  }
+});
+
+app.delete('/api/toy/:toyId', async (req, res) => {
+  try {
+    const { toyId } = req.params;
+    const msg = await toyService.remove(toyId);
+    res.send({ msg, toyId });
+  } catch (err) {
+    loggerService.error('Cannot delete toy', err);
+    res.status(400).send('Cannot delete toy, ' + err.message || 'Unknown error');
+  }
+});
 
 // Fallback
 app.get('/**', (req, res) => {
-  res.sendFile(path.resolve('public/index.html'))
-})
+  res.sendFile(path.resolve('public/index.html'));
+});
 
 // Listen will always be the last line in our server!
-const port = 3030
+const port = 3030;
 app.listen(port, () => {
-  loggerService.info(`Server listening on port http://127.0.0.1:${port}/`)
-})
+  loggerService.info(`Server listening on port http://127.0.0.1:${port}/`);
+});
+
