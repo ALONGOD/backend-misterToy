95771fee98bdb35ea9db72e23aef7553b85cea25
ALONGOD <alongoddard1@gmail.com>
2024-08-14 11:06:58 +0300

    Add working js server to support sockets

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import http from 'http'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import cors from 'cors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import { setupSocketAPI } from './services/socket.service.js'
+import { logger } from './services/logger.service.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-import { logger } from './services/logger.service.js'
 logger.info('server.js loaded...')
 
 const app = express()
+const server = http.createServer(app) // Create an HTTP server
 
 // Express App Config
 app.use(cookieParser())
@@ -18,18 +21,14 @@ app.use(express.json())
 app.use(express.static('public'))
 
 if (process.env.NODE_ENV === 'production') {
-  // Express serve static files on production environment
+  // Serve static files in production
   app.use(express.static(path.resolve(__dirname, 'public')))
-  console.log('__dirname: ', __dirname)
 } else {
   // Configuring CORS
-  // Make sure origin contains the url 
-  // your frontend dev-server is running on
   const corsOptions = {
     origin: [
       'http://127.0.0.1:5173',
       'http://localhost:5173',
-
       'http://127.0.0.1:3000',
       'http://localhost:3000',
     ],
@@ -38,27 +37,27 @@ if (process.env.NODE_ENV === 'production') {
   app.use(cors(corsOptions))
 }
 
+// Routes
 import { authRoutes } from './api/auth/auth.routes.js'
 import { userRoutes } from './api/user/user.routes.js'
 import { toyRoutes } from './api/toy/toy.routes.js'
 import { reviewRoutes } from './api/review/review.routes.js'
 
-// routes
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/toy', toyRoutes)
 app.use('/api/review', reviewRoutes)
 
-// Make every unmatched server-side-route fall back to index.html
-// So when requesting http://localhost:3030/index.html/toy/123 it will still respond with
-// our SPA (single page app) (the index.html file) and allow vue-router to take it from there
-
+// Handle unmatched routes to serve the SPA
 app.get('/**', (req, res) => {
   res.sendFile(path.resolve('public/index.html'))
 })
 
+// Setup Socket.IO
+setupSocketAPI(server) // Integrate Socket.IO with the HTTP server
+
 const port = process.env.PORT || 3030
 
-app.listen(port, () => {
+server.listen(port, () => {
   logger.info('Server is running on port: ' + port)
 })
